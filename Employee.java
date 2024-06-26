876fa37f3195af52c82f9d2755ceb48303678d2b
İrem Bodur <irembodur99@gmail.com>
2024-06-26 10:11:55 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,25 +1,38 @@
 package Merve_Hoca_Mext.Day2;
 
 public class Employee {
-    private String name;
-    protected double salary;
-    private int age;
 
-    public Employee(String name, double salary, int age){
-        this.name = name;
-        this.salary = salary;
-        this.age = age;
+    //Çalışanın adını tutan bir metin değişkenidir.
+    private String name; // "private" bu değişkenin sadece bu sınıfın içinden erişilebilir olduğunu belirtir.
+
+    //Çalışanın  maaşını tutan ondalıklı sayı değişkeni
+    protected double salary; // Bu değişkene aynı paket içerisinden veya alt sınıflardan erişilebilir.
+
+    //Çalışanın yaşını tutan tam sayı değişkeni.
+    private int age; // "private"
+
+    public Employee(String name, double salary, int age) { //Bu sınıfın bir örneği oluşturulurken çağrılan metottur.
+        this.name = name; // "name" parametresini sınıfın "name" değişkenine atar.
+        this.salary = salary; // "salary" parametresini sınıfın "salary" değişkenine atar.
+        this.age = age; // "age" parametresini aınıfın "age" değişkenine atar.
     }
 
-    public String getName(){
+    // Getter ve Setter Metotları:
+    // Bu metotlar, özel değişkenlerin ddeğerlerini dış dünyaya(başka sınıflara yani) vermek için kullanılır.
+    public String getName() {
         return this.name;
+
     }
 
-    public int getAge(){
+    protected double getSalary() {
+        return this.salary;
+    }
+
+    private int getAge() {
         return this.age;
     }
 
-    public double getSalary(){
-        return this.salary;
+    public void raiseSalary() { //Çalışanın maaşını arttıran metot.
+        this.salary = this.salary * 1.2; // %20 değerinde maaşı arttırır.
     }
 }
--- Salesperson.java
+++ Salesperson.java
@@ -1,11 +1,13 @@
 package Merve_Hoca_Mext.Day2;
 
 public class Salesperson extends Employee {
+    // "extends" Employee, Salesperson classını (sınıfını) Employee sınıfından miras aldığını belirtir.
+    //    // "Employee" sınıfından miras aldığını belirtir. Yani " salesperson" sınıfının tüm özelliklerine ve metotlarına sahip olur.
     private double comissionPercentage;
 
     public Salesperson(String name, double salary, int age,
                        double comissionPercentage) {
-        super(name, salary, age);
+        super(name, salary, age); // name, salary ve age i başka sınıftan aldığımız için super'i kullandık.
         this.comissionPercentage = comissionPercentage;
     }
 
